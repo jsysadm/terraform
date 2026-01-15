0aa6ad8f01614afddf6e7b573348dc01bd4a66d1
zaraza7 <zakixas@gmail.com>
2026-01-15 14:06:17 +0000

    Initial commit

--- Proxmox/lxc-instance.tf
+++ Proxmox/lxc-instance.tf
@@ -2,13 +2,13 @@ resource "proxmox_lxc" "lxc-instance" {
   target_node  = "pve"                  # -- Keisk į savo Proxmox mazgo/host pavadinimą
   hostname     = "lxc-instance"         # -- Konteinerio vardas
   ostemplate   = "ISO:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst" # -- nurodyti LXC template PVE [storage]:vztmpl/[template_name]
-  password     = var.lxc_password       # -- kintamasis faile variables.tf, lauko lxc_passwd tipas,  failas terraform.auto.tfvars ir lxc_password  = "slaptažodis"
+  password     = var.lxc_password       # -- kintamasis faile variables.tf, lauko lxc_passwd tipas,  failas terraform.tfvars ir lxc_password  = "slaptažodis"
   unprivileged = true
   cores        = 2
   memory       = 2048
   swap         = 512
   start        = true
-  ssh_public_keys = var.PUBLIC_SSH_KEY  # -- kintamasis faile variables.tf, lauko PUBLIC_SSH_KEY tipas, failas terraform.auto.tfvars ir PUBLIC_SSH_KEY  = "pub_key"
+  ssh_public_keys = var.PUBLIC_SSH_KEY  # -- kintamasis faile variables.tf, lauko PUBLIC_SSH_KEY tipas, failas terraform.tfvars ir PUBLIC_SSH_KEY  = "pub_key"
   tags = "terraform"                    # -- PVE naudojamas TAG
   rootfs {
     storage = "pve_storage_name"                  # -- disko pavadinimas, kuriame talpinama LXC virtualus diskas
--- Proxmox/provider.tf
+++ Proxmox/provider.tf
@@ -1,7 +1,8 @@
 terraform {
     required_providers {
         proxmox = {
-            source = "telmate/proxmox"
+            source  = "Telmate/proxmox"
+            version = "v3.0.2-rc07" # -- patikrinkite naujausią versiją https://registry.terraform.io/providers/Telmate/proxmox/latest
         }
     }
 }
@@ -9,6 +10,6 @@ terraform {
 provider "proxmox" {
     pm_api_url          = "https://172.16.1.100:8006/api2/json"   # -- proxmox ipv4
     pm_api_token_id     = "terraform@pam!terraform"                 # -- pve token id "[user]![Token ID]", PVE:Edit: Token: Privilege Separation []
-    pm_api_token_secret = var.proxmox_token                         # -- kintamasis faile variables.tf, lauko proxmox_token tipas,  failas terraform.auto.tfvars ir proxmox_token  = "token"
+    pm_api_token_secret = var.proxmox_token                         # -- kintamasis faile variables.tf, lauko proxmox_token tipas,  failas terraform.tfvars ir proxmox_token  = "token"
     pm_tls_insecure     = true                                      # -- true jeigu https be sertifikato
 }
